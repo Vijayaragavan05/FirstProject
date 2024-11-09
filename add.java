af93d2a6e07bb9e19f8f10f840115d95e9ea3dea
Vijayaragavan05 <vijayaragavan4818@gmail.com>
2024-11-09 21:58:02 +0530

    Update add.java
    Signed-off-by: Vijayaragavan05 <vijayaragavan4818@gmail.com>

--- add.java
+++ add.java
@@ -1,5 +1,9 @@
  class Hello{
     public static void main(String args[]){
         System.out.println("Hello Everyone");
+        a= 10;
+        b=20;
+        c=b-a;
+     System.out.println(c);
     }
 }
